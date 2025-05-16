75667647888e5ce4196343b1e16bc11357ed0045
John Serrano <andrey_201015@hotmail.com>
2025-05-16 17:36:10 -0500

    Add get secure user

---
+++ src/lib/auth/server.ts
@@ -0,0 +1,26 @@
+import { auth, currentUser } from '@clerk/nextjs/server'
+
+export async function getSecureUser() {
+  const user = await currentUser()
+
+  if (!user) {
+    return {
+      id: process.env.DEFAULT_USER_ID ?? '',
+      fullName: 'Guest',
+    }
+  }
+
+  return user
+}
+
+export async function getSecureSession() {
+  const session = await auth()
+
+  if (!session.userId) {
+    return {
+      userId: process.env.DEFAULT_USER_ID ?? '',
+    }
+  }
+
+  return session
+}
