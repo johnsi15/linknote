17bcca8985e9f23eb312666695b373f2ed524fba
John Serrano <andrey_201015@hotmail.com>
2025-07-03 21:00:36 -0500

    Add semancti HTML

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -14,12 +14,12 @@ export default async function DashboardPage() {
 
   return (
     <div className='space-y-8'>
-      <div className='flex items-center justify-between'>
+      <section className='flex items-center justify-between'>
         <h1 className='text-3xl font-bold tracking-tight'>Dashboard</h1>
         <NewLinkButton />
-      </div>
+      </section>
 
-      <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+      <section className='grid grid-cols-1 md:grid-cols-3 gap-6'>
         <div className='md:col-span-2 space-y-6'>
           <div className='space-y-4'>
             <div className='flex items-center justify-between'>
@@ -106,7 +106,7 @@ export default async function DashboardPage() {
             </div>
           </div>
         </div>
-      </div>
+      </section>
     </div>
   )
 }
